c090ae248546debad533b3c085eb8383599dbf50
eelaix <eelaix@gmail.com>
2021-08-14 05:36:18 +0800

    ok

--- src/config.js
+++ src/config.js
@@ -2,9 +2,9 @@
     export const APPVERSION = '1.0.0';
     
     // 后台API部署地址
-    export const BASE = 'https://e.mosf.cn/webmgr';
+    export const BASE = 'https://some.site.com/webmgr';
     //export const BASE = 'http://127.0.0.1:3000';
 
     //存放二维码的URL地址
-    export const QRSITE = 'https://a.mosf.cn';
+    export const QRSITE = 'https://some.domain.com';
     
